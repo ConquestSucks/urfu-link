01bcef9b10ab56502e8c9f0affd427180bdc5e8b
ConquestSucks <91684467+ConquestSucks@users.noreply.github.com>
2026-03-29 23:41:59 +0500

    fix(frontend): added phosphor-icons and fix notifications skeleton

--- apps/client/src/entities/inbox-notification/ui/InboxNotificationSkeleton.tsx
+++ apps/client/src/entities/inbox-notification/ui/InboxNotificationSkeleton.tsx
@@ -4,9 +4,7 @@ import { View } from "react-native";
 export const InboxNotificationSkeleton = () => {
     return (
         <View className="flex-row gap-3 px-4 py-3 md:rounded-2xl select-none border-b md:border border-white/5 md:bg-white/5">
-            <View className="flex-shrink-0 flex items-center justify-center text-white bg-[#1A1D27] shadow-sm w-12 h-12 rounded-[14px]">
-                <View className="h-6 w-6 rounded bg-white/10 animate-pulse" />
-            </View>
+            <View className="flex-shrink-0 flex items-center justify-center text-white bg-[#1A1D27] shadow-sm w-12 h-12 rounded-[14px]"></View>
 
             <View className="min-w-0 flex-1 gap-2">
                 <View className="flex-row justify-between gap-2">
